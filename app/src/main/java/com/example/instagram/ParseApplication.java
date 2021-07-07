ddb11b4065d6c4f29154e9b08499dd79b8b16f67
Paulina LH <paulinalh2001@gmail.com>
2021-07-07 17:25:21 -0500

    Sign up + Login + User Persistence

---
+++ app/src/main/java/com/example/instagram/ParseApplication.java
@@ -0,0 +1,21 @@
+package com.example.instagram;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+public class ParseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("5f8v7x9KwYMMspN9n2N7m48I9uDsjEsLx9Pvf3r9")
+                .clientKey("6jjgzD1D58iGY0tEVpfLLWR3cf4BlrN0gqUd8fy2")
+                .server("https://parseapi.back4app.com")
+                .build()
+        );
+    }
+}
