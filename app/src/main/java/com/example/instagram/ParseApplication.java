50489bc33de224972356bfddb0688c0123a7f1ae
Paulina LH <paulinalh2001@gmail.com>
2021-07-08 17:32:04 -0500

    Posting Photos

--- app/src/main/java/com/example/instagram/ParseApplication.java
+++ app/src/main/java/com/example/instagram/ParseApplication.java
@@ -11,6 +11,9 @@ public class ParseApplication extends Application {
     public void onCreate() {
         super.onCreate();
 
+        //register your parse models
+        ParseObject.registerSubclass(Post.class);
+
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("5f8v7x9KwYMMspN9n2N7m48I9uDsjEsLx9Pvf3r9")
                 .clientKey("6jjgzD1D58iGY0tEVpfLLWR3cf4BlrN0gqUd8fy2")
